b38f0c43b45db57e6fd13a95a4772a25ebd5c672
sugarvag <sugarvag@gmail.com>
2023-10-03 12:51:23 +0300

    Added error detection, to prevent app from hanging when an API timeout occurs, or any error while sending data. Also added error message for the Air API.

--- main.py
+++ main.py
@@ -4,80 +4,9 @@ import spotipy
 import requests
 import configparser
 import webbrowser
+import requests.exceptions
 
-# Function to update the song info on the Shoutcast server
-def update_song_info():
-    global is_sending, send_to_shoutcast, send_to_air_api
-    if not is_sending:
-        is_sending = True
-        begin_stop_button.config(text="Stop Sending")
-        update_song()
-    else:
-        is_sending = False
-        begin_stop_button.config(text="Begin Sending")
-
-# Function to update the currently playing song info
-def update_song():
-    current_track = sp.current_playback()
-    if current_track is not None:
-        track_name = current_track['item']['name']
-        artist_name = current_track['item']['artists'][0]['name']
-        song_info = f'{artist_name} - {track_name}'
-        song_label.config(text=song_info)
-
-        global last_song_info
-        if is_sending and song_info != last_song_info:
-            # Only update if the song has changed
-            last_song_info = song_info
-
-            if send_to_shoutcast:
-                # Construct the URL for updating the song info on the Shoutcast server
-                shoutcast_url = f'http://{shoutcast_server}:{shoutcast_port}/admin.cgi?pass={admin_pass}&mode=updinfo&song={song_info}'
-
-                # Send the HTTP GET request to update the song info
-                response = requests.get(shoutcast_url)
-
-                if response.status_code == 200:
-                    print(f'Successfully updated song info on Shoutcast server: {song_info}')
-                    error_label.config(text="")
-                else:
-                    print(f'Failed to update song info on Shoutcast server. HTTP Status Code: {response.status_code}')
-                    error_label.config(text="Shoutcast Server Error")
-
-            if send_to_air_api:
-                # Construct the URL for updating the song info on the TuneIn Air API
-                air_api_url = f'http://air.radiotime.com/Playing.ashx?partnerId={partner_id}&partnerKey={partner_key}&id={station_id}&title={track_name}&artist={artist_name}'
-
-                # Send the HTTP GET request to update the song info
-                response = requests.get(air_api_url)
-
-                if response.status_code == 200:
-                    print(f'Successfully updated song info on TuneIn Air API: {song_info}')
-                else:
-                    print(f'Failed to update song info on TuneIn Air API. HTTP Status Code: {response.status_code}')
-
-    if is_sending:
-        root.after(10000, update_song)  # Update song info every 10 seconds while sending is active
-        root.after(10000, check_shoutcast)  # Check Shoutcast server status every 10 seconds
-
-# Function to check the Shoutcast server status
-def check_shoutcast():
-    if send_to_shoutcast:
-        try:
-            response = requests.head(f'http://{shoutcast_server}:{shoutcast_port}/admin.cgi')
-            if response.status_code != 200:
-                error_label.config(text="Shoutcast Server OK")
-                root.after(10000, check_shoutcast)  # Retry after 10 seconds
-            else:
-                error_label.config(text="")
-        except requests.ConnectionError:
-            error_label.config(text="Shoutcast Server Error")
-            root.after(10000, check_shoutcast)  # Retry after 10 seconds
-    else:
-        error_label.config(text="")  # Clear the error label if not sending to Shoutcast
-
-
-# Function to toggle sending to Shoutcast
+# Function to toggle sending to Shoutcast Server
 def toggle_shoutcast():
     global send_to_shoutcast
     send_to_shoutcast = not send_to_shoutcast
@@ -86,6 +15,104 @@ def toggle_shoutcast():
 def toggle_air_api():
     global send_to_air_api
     send_to_air_api = not send_to_air_api
+# Function to update the song info on the Shoutcast server
+def update_song_info():
+    global is_sending, send_to_shoutcast, send_to_air_api, error_label_air_api, error_label_shoutcast
+
+    # Clear error labels
+    error_label_shoutcast.config(text='')
+    error_label_air_api.config(text='')
+
+    if not is_sending:
+        is_sending = True
+        begin_stop_button.config(text="Stop Sending")
+
+        # Disable the checkboxes when sending is enabled
+        shoutcast_checkbox.config(state="disabled")
+        air_api_checkbox.config(state="disabled")
+
+        update_song()
+    else:
+        is_sending = False
+        begin_stop_button.config(text="Begin Sending")
+
+        # Re-enable the checkboxes when sending is not active
+        shoutcast_checkbox.config(state="normal")
+        air_api_checkbox.config(state="normal")
+
+# Function to update the currently playing song info
+def update_song():
+    try:
+        current_track = sp.current_playback()
+        if current_track is not None:
+            track_name = current_track['item']['name']
+            artist_name = current_track['item']['artists'][0]['name']
+            song_info = f'{artist_name} - {track_name}'
+            song_label.config(text=song_info)
+
+            global last_song_info
+            if is_sending and song_info != last_song_info:
+                # Only update if the song has changed
+                last_song_info = song_info
+
+                if send_to_shoutcast:
+                    # Construct the URL for updating the song info on the Shoutcast server
+                    shoutcast_url = f'http://{shoutcast_server}:{shoutcast_port}/admin.cgi?pass={admin_pass}&mode=updinfo&song={song_info}'
+
+                    try:
+                        # Send the HTTP GET request to update the song info
+                        response = requests.get(shoutcast_url)
+
+                        if response.status_code == 200:
+                            print(f'Successfully updated song info on Shoutcast server: {song_info}')
+                            error_label_shoutcast.config(text="")
+                        else:
+                            print(f'Failed to update song info on Shoutcast server. HTTP Status Code: {response.status_code}')
+                            error_label_shoutcast.config(text="*** Shoutcast Server Error ***")
+                    except requests.exceptions.RequestException as e:
+                        print(f'Failed to send data to Shoutcast server: {str(e)}')
+                        error_label_shoutcast.config(text="*** Shoutcast Server Error ***")
+
+                if send_to_air_api:
+                    # Construct the URL for updating the song info on the TuneIn Air API
+                    air_api_url = f'http://air.radiotime.com/Playing.ashx?partnerId={partner_id}&partnerKey={partner_key}&id={station_id}&title={track_name}&artist={artist_name}'
+
+                    try:
+                        # Send the HTTP GET request to update the song info
+                        response = requests.get(air_api_url)
+
+                        if response.status_code == 200:
+                            print(f'Successfully updated song info on TuneIn Air API: {song_info}')
+                            error_label_air_api.config(text="")
+                        else:
+                            print(f'Failed to update song info on TuneIn Air API. HTTP Status Code: {response.status_code}')
+                            error_label_air_api.config(text="*** Air API Error ***")
+                    except requests.exceptions.RequestException as e:
+                        print(f'Failed to send data to TuneIn Air API: {str(e)}')
+                        error_label_air_api.config(text="*** Air API Error ***")
+
+        if is_sending:
+            root.after(10000, update_song)  # Update song info every 10 seconds while sending is active
+            root.after(10000, check_shoutcast)  # Check Shoutcast server status every 10 seconds
+    except spotipy.SpotifyException as e:
+        print(f'Spotipy Error: {str(e)}')
+        error_label_shoutcast.config(text="Spotify API Error")
+
+# Function to check the Shoutcast server status
+def check_shoutcast():
+    if send_to_shoutcast:
+        try:
+            response = requests.head(f'http://{shoutcast_server}:{shoutcast_port}/admin.cgi')
+            if response.status_code != 200:
+                error_label_shoutcast.config(text="Shoutcast Server OK")
+                root.after(10000, check_shoutcast)  # Retry after 10 seconds
+            else:
+                error_label_shoutcast.config(text="")
+        except requests.ConnectionError:
+            error_label_shoutcast.config(text="Shoutcast Server Error")
+            root.after(10000, check_shoutcast)  # Retry after 10 seconds
+    else:
+        error_label_shoutcast.config(text="")  # Clear the error label if not sending to Shoutcast
 
 # Function to open the GitHub repository link
 def open_github_link():
@@ -97,28 +124,35 @@ def exit_app():
 
 # Read Spotify API credentials and Shoutcast server details from config.txt
 config = configparser.ConfigParser()
-config.read('config.txt')
+try:
+    config.read('config.txt')
 
-spotify_config = config['spotify']
-client_id = spotify_config['client_id']
-client_secret = spotify_config['client_secret']
-redirect_uri = spotify_config['redirect_uri']
+    spotify_config = config['spotify']
+    client_id = spotify_config.get('client_id')
+    client_secret = spotify_config.get('client_secret')
+    redirect_uri = spotify_config.get('redirect_uri')
 
-shoutcast_config = config['shoutcast']
-shoutcast_server = shoutcast_config['server']
-shoutcast_port = shoutcast_config['port']
-admin_pass = shoutcast_config['admin_pass']
+    shoutcast_config = config['shoutcast']
+    shoutcast_server = shoutcast_config.get('server')
+    shoutcast_port = shoutcast_config.get('port')
+    admin_pass = shoutcast_config.get('admin_pass')
 
-# Read AIR API details from config.txt
-air_config = config['air_api']
-partner_id = air_config['partner_id']
-partner_key = air_config['partner_key']
-station_id = air_config['station_id']
+    # Read AIR API details from config.txt
+    air_config = config['air_api']
+    partner_id = air_config.get('partner_id')
+    partner_key = air_config.get('partner_key')
+    station_id = air_config.get('station_id')
+
+except (configparser.NoSectionError, KeyError) as e:
+    print(f'Error reading configuration data: {str(e)}')
+    error_label_shoutcast.config(text="Configuration Error")
+    sp = None
 
 # Initialize Spotipy with OAuth2 authentication
 try:
     sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri, scope="user-read-playback-state user-read-currently-playing"))
 except spotipy.oauth2.SpotifyOauthError as e:
+    print(f'Spotipy Error: {str(e)}')
     error_label.config(text="Spotify Connection Error")
     sp = None
 
@@ -147,8 +181,12 @@ shoutcast_checkbox.pack()
 air_api_checkbox = tk.Checkbutton(root, text="TuneIn Air API", command=toggle_air_api)
 air_api_checkbox.pack()
 
-error_label = tk.Label(root, text="", fg="blue")
-error_label.pack()
+# Create labels for Shoutcast Server and Air API errors
+error_label_shoutcast = tk.Label(root, text="", fg="red")
+error_label_shoutcast.pack()
+
+error_label_air_api = tk.Label(root, text="", fg="red")
+error_label_air_api.pack()
 
 # Create a Text widget for the footer as a hyperlink
 footer_text = tk.Text(root, wrap="none", height=1, width=50, cursor="hand2", relief=tk.FLAT, borderwidth=0)
